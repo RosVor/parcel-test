b88077a9f7cce3609a4c4796cb04f2f10112028a
RosVor <101742140+RosVor@users.noreply.github.com>
2023-08-06 17:52:56 +0000

    Deploying to gh-pages from @ RosVor/parcel-test@d5e3e7a38de1d3e2a9239a59eeecae7b7efc456d 🚀

--- index.94a9a5ad.js
+++
@@ -1,2 +0,0 @@
-fetch("https://books-backend.p.goit.global/books/category-list",{method:"GET",headers:{accept:"application/json"}}).then((function(o){if(!o.ok)throw new Error(o.statusText);return o.json()})).then((function(o){console.log(o)})).catch((function(o){console.error("Error:",o)}));
-//# sourceMappingURL=index.94a9a5ad.js.map
--- index.f23f7b4d.js
+++
@@ -1,2 +0,0 @@
-fetch("https://books-backend.p.goit.global/books/category-list",{method:"GET",headers:{accept:"application/json"}}).then((o=>{if(!o.ok)throw new Error(o.statusText);return o.json()})).then((o=>{console.log(o)})).catch((o=>{console.error("Error:",o)}));
-//# sourceMappingURL=index.f23f7b4d.js.map
