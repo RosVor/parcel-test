24bf94af52167f45569522574e71bfa89996ac64
RosVor <101742140+RosVor@users.noreply.github.com>
2023-08-06 17:47:21 +0000

    Deploying to gh-pages from @ RosVor/parcel-test@76417bdf83d7208ddd0be2c44a909e6d378dbf2a 🚀

---
+++ index.b433a77f.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector(".categories-list").querySelectorAll(".categories-text");e.forEach((function(t){t.addEventListener("click",(function(n){n.preventDefault(),e.forEach((function(e){e.classList.remove("current")})),t.classList.add("current")}))}))}));
+//# sourceMappingURL=index.b433a77f.js.map
